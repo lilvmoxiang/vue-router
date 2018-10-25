4accd539c245eb37b31f46e60587e5b4ba31a521
lilvmoxiang <15501054205@163.com>
2018-10-25 14:32:05 +0800

    第一次提交代码

---
+++ src/admin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="admin">
+  	<div class="nav" v-for="item in routes">
+  		<router-link v-for="(child,index) in item.children" :key="index" :to="{path:item.path+''+ child.path}">
+  			{{ child.alias }}
+  		</router-link>
+  	</div>
+  	<section>
+  		<router-view></router-view>
+  	</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin',
+  data(){
+  	return {
+  		routes:this.$router.options.routes
+  	}
+  }
+}
+</script>
+
+<style>
+#admin {
+  width:100%;
+  height:100%;
+  background:#ccc; 
+}
+#admin .nav{
+  	width:100%;
+  	height:80px;
+  	text-align:center;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Admin from '../admin';
+
+import Index from '../client/index';
+
+import Page1 from '../client/page/page1';
+import Page2 from '../client/page/page2';
+Vue.use(Router)
+
+const router = new Router({
+  mode:'hash',
+  routes: [
+    {
+      path: '/',//默认显示的内容页面路径
+      name: 'admin',
+      component: Admin,
+      children:[
+      	{
+      		path:'index',
+      		name:'index',
+      		alias:'首页',
+      		component:Index,
+      		children:[
+      			{
+      				path:'page1',
+      				name:'page1',
+      				alias:'page1页面',
+      				component:Page1
+      			},
+      			{
+      				path:'page2',
+      				name:'page2',
+      				alias:'page2页面',
+      				component:Page2
+      			}
+      		]
+      	}
+      ]
+    }
+  ]
+})
+export default router;
